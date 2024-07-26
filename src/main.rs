39611704900480afe3d32ed2620c799cf7fd02e4
9Cr13CoMoV <114803384+9Cr13CoMoV@users.noreply.github.com>
2024-07-26 13:31:52 -0700

    first fix

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use bevy::prelude::VariableCurve;
 mod lib;
 
 fn main() {
-    println!("{}", test(23));
+    println!("{}", test(2));
 }
 
-fn test(variable:i32 ) -> f32 {
-    if variable == 23 {return variable as f32;}
-    else {variable + 23;
-    variable as f32}
+fn test(mut variable: i32 ) -> f32 {
+    if variable == 2 {return variable as f32;}
+    else {
+        variable += variable + 2;
+        variable as f32
+    }
 }
