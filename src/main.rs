a928c0a81da3e9042fe0cd326c2f406fef168db7
9Cr13CoMoV <114803384+9Cr13CoMoV@users.noreply.github.com>
2024-07-26 13:46:16 -0700

    mutable demo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use bevy::prelude::*;
-mod lib;
 
+mod lib;
 fn main() {
-    println!("{}", test(2));
+    println!("{}", test1(4));
+    println!("{}", test2(4));
 }
 
-fn test(mut variable: i32 ) -> f32 {
+fn test1(param: i32) -> i32 {
+    if param == 2 { return param; }
+    // can't do this:
+    // param += 2;
+    // param is not modified or returned: new value (param +2) is constructed:
+    return param + 2;
+}
+fn test2(mut variable: i32 ) -> f32 {
     if variable == 2 {return variable as f32;}
     else {
         variable += variable + 2;
