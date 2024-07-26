286c347cd5cc0634ff74f57dc5b656baaaf82f40
9Cr13CoMoV <114803384+9Cr13CoMoV@users.noreply.github.com>
2024-07-26 13:47:42 -0700

    var->param

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,11 @@ fn test1(param: i32) -> i32 {
     // param is not modified or returned: new value (param +2) is constructed:
     return param + 2;
 }
-fn test2(mut variable: i32 ) -> f32 {
-    if variable == 2 {return variable as f32;}
+// mut makes the param
+fn test2(mut param: i32 ) -> f32 {
+    if param == 2 {return param as f32;}
     else {
-        variable += variable + 2;
-        variable as f32
+        param += param + 2;
+        param as f32
     }
 }
